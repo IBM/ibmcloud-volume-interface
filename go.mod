b664c9055b04ed6deb798832beaff4a8a7dfa62b
Guna K Kambalimath <Guna.Kambalimath@ibm.com>
2022-10-19 15:10:45 +0530

    Testing sidecar backward compatibility

--- go.mod
+++ go.mod
@@ -5,8 +5,8 @@ go 1.15
 require (
 	github.com/BurntSushi/toml v1.0.0
 	github.com/IBM-Cloud/ibm-cloud-cli-sdk v0.6.7
-	github.com/IBM/secret-common-lib v1.0.4
-	github.com/IBM/secret-utils-lib v1.0.3
+	github.com/IBM/secret-common-lib v1.0.5-0.20221019084743-b7fe6c885a5a
+	github.com/IBM/secret-utils-lib v1.0.4-0.20221019084326-0a031b3c721c
 	github.com/golang-jwt/jwt/v4 v4.2.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/prometheus/client_golang v1.7.1
--- provider/iam/token_exchange.go
+++ provider/iam/token_exchange.go
@@ -127,7 +127,7 @@ func (tes *tokenExchangeService) ExchangeIAMAPIKeyForIMSToken(iamAPIKey string, 
 // ExchangeIAMAPIKeyForAccessToken ...
 func (tes *tokenExchangeService) ExchangeIAMAPIKeyForAccessToken(iamAPIKey string, logger *zap.Logger) (*AccessToken, error) {
 	logger.Info("Fetching using secret provider")
-	token, _, err := tes.secretprovider.GetDefaultIAMToken("reason", false)
+	token, _, err := tes.secretprovider.GetDefaultIAMToken(false)
 	if err != nil {
 		logger.Error("Error fetching iam token", zap.Error(err))
 		return nil, err
