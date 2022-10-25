87c0753e8bd50de7b9b0f200fcefe818ddfca033
Guna K Kambalimath <Guna.Kambalimath@ibm.com>
2022-10-25 15:33:13 +0530

    Optional providerType

--- provider/iam/token_exchange.go
+++ provider/iam/token_exchange.go
@@ -34,6 +34,15 @@ import (
 	sp "github.com/IBM/secret-utils-lib/pkg/secret_provider"
 )
 
+const (
+	// VPC - if VPC option is provided as providerType, key will read from VPC section.
+	VPC = secret_provider.VPC
+	// Bluemix - if Bluemix option is provided as providerType, key will read from Bluemix section.
+	Bluemix = secret_provider.Bluemix
+	// Softlayer - if Softlayer option is provided as providerType, key will read from Softlayer section.
+	Softlayer = secret_provider.Softlayer
+)
+
 // tokenExchangeService ...
 type tokenExchangeService struct {
 	authConfig     *AuthConfiguration
@@ -60,13 +69,17 @@ func NewTokenExchangeServiceWithClient(authConfig *AuthConfiguration, httpClient
 }
 
 // NewTokenExchangeService ...
-func NewTokenExchangeService(authConfig *AuthConfiguration) (TokenExchangeService, error) {
+func NewTokenExchangeService(authConfig *AuthConfiguration, providerType ...string) (TokenExchangeService, error) {
 	httpClient, err := config.GeneralCAHttpClient()
 	if err != nil {
 		return nil, err
 	}
-	providerTypeArg := map[string]string{
-		secret_provider.ProviderType: secret_provider.VPC,
+	var providerTypeArg map[string]string
+
+	if len(providerType) != 0 {
+		providerTypeArg[secret_provider.ProviderType] = providerType[0]
+	} else {
+		providerTypeArg[secret_provider.ProviderType] = VPC
 	}
 	spObject, err := secret_provider.NewSecretProvider(providerTypeArg)
 	if err != nil {
